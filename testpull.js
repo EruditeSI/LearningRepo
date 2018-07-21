490f690e1f71aa3efc9ec0ae4ecb04bc377b7cf4
Louai Othman <lo7847@att.net>
2018-07-21 12:59:23 +0300

    fixd confilict 2

--- testpull.js
+++ testpull.js
@@ -1,12 +1,2 @@
-<<<<<<< HEAD
 test
-<<<<<<< HEAD
-What
-=======
-shit this is to complicated bra
->>>>>>> 66739e0aa5333413703121caa446ad449b271bdf
-=======
-
--test
--shit this is to complicated bra!
->>>>>>> df0b8a750043a3e46dad9e3ad23adc0800e92cc7
+shit this is to complicated bra!
